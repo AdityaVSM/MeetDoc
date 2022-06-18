ce1ec6c7931d0b2d9ba34695c867e46f84293c5d
adigapranava <pranavaadiga.is19@bmsce.ac.in>
2022-06-18 13:38:23 +0530

    login and doctor sign in and signup done

--- app/src/main/java/com/example/meetdoc/SiginActivity.java
+++ app/src/main/java/com/example/meetdoc/SiginActivity.java
@@ -2,16 +2,13 @@ package com.example.meetdoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SiginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(MainActivity.this, SiginActivity.class));
+        setContentView(R.layout.activity_sigin);
     }
 }
