0aab71b7f6a22546e41b0c622967382bed4884e6
adigapranava <pranavaadiga.is19@bmsce.ac.in>
2022-06-18 13:44:58 +0530

    login and doctor sign in and signup done

---
+++ app/src/main/java/com/example/meetdoc/doctorSignup.java
@@ -0,0 +1,14 @@
+package com.example.meetdoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doctorSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_signup);
+    }
+}
