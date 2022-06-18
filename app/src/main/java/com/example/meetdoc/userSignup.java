8578fff4fce96f11495e845ce4a91e6b5dc44036
adigapranava <pranavaadiga.is19@bmsce.ac.in>
2022-06-18 15:53:01 +0530

    user Signup done

--- app/src/main/java/com/example/meetdoc/userSignup.java
+++ app/src/main/java/com/example/meetdoc/userSignup.java
@@ -2,13 +2,66 @@ package com.example.meetdoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class userSignup extends AppCompatActivity {
+import java.util.Calendar;
 
+public class userSignup extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,  AdapterView.OnItemSelectedListener {
+    String[] bloodGroup = { "A+", "A-", "B+", "B-", "O+", "O-", "AB+","AB-"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_signup);
+
+        findViewById(R.id.dobLayout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+        Spinner spin = (Spinner) findViewById(R.id.bloodgroup);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,bloodGroup);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+    }
+
+    private void  showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        TextView dob= (TextView) findViewById(R.id.dob);
+        dob.setText(String.valueOf(dayOfMonth) + "/"+String.valueOf(month)+"/" +String.valueOf(year));
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
