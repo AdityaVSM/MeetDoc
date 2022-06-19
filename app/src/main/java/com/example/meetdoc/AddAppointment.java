72d4f2246870f9e64fc12556fe29d6ce3b00ff9f
adigapranava <pranavaadiga.is19@bmsce.ac.in>
2022-06-19 06:46:33 +0530

    doctor work aaytu

--- app/src/main/java/com/example/meetdoc/AddAppointment.java
+++ app/src/main/java/com/example/meetdoc/AddAppointment.java
@@ -1,5 +1,6 @@
 package com.example.meetdoc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,6 +12,11 @@ import android.widget.Toast;
 import com.example.meetdoc.Adapter.DoctorsAdapter;
 import com.example.meetdoc.Adapter.Specialization;
 import com.example.meetdoc.Models.Doctor;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -18,8 +24,9 @@ public class AddAppointment extends AppCompatActivity {
     Spinner specializationView, doctorsView;
     String specializationList[] = {"Dentist","Cardiologist","ENT","General"};
     int spcializationImgs[] = {R.drawable.tooth, R.drawable.heart, R.drawable.ear, R.drawable.human};
-
     ArrayList<Doctor> doctors = new ArrayList<>();
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +43,14 @@ public class AddAppointment extends AppCompatActivity {
         DoctorsAdapter doctorsAdapter = new DoctorsAdapter(getApplicationContext(), doctors);
         doctorsView.setAdapter(doctorsAdapter);
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
         specializationView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                String specialization_chosen = specializationList[position].toString();
+                getDoctorDetails(database, specialization_chosen);
             }
 
             @Override
@@ -51,7 +62,7 @@ public class AddAppointment extends AppCompatActivity {
         doctorsView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), doctors.get(position).getName(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "doctors.get(position).getName()", Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -60,5 +71,32 @@ public class AddAppointment extends AppCompatActivity {
             }
         });
 
+
+}
+
+    private void getDoctorDetails(FirebaseDatabase database, String specialization_chosen) {
+        doctors.clear();
+        database.getReference().child("doctor").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot data_snapshot : snapshot.getChildren()){
+                    Doctor curr_doctor = new Doctor();
+                    curr_doctor = data_snapshot.getValue(Doctor.class);
+                    System.out.println("in"+specialization_chosen);
+                    System.out.println("curr"+curr_doctor.getSpecialization());
+                    if(specialization_chosen.equals(curr_doctor.getSpecialization().toString())) {
+                        System.out.println("doctor for particular spec"+curr_doctor.getName().toString());
+                        doctors.add(curr_doctor);
+                    }
+                }
+                DoctorsAdapter doctorsAdapter = new DoctorsAdapter(getApplicationContext(), doctors);
+                doctorsView.setAdapter(doctorsAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
